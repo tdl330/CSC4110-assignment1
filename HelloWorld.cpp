45fa109523fd1922deb850c06fca81215872c1d2
Trevor Lee <tdl330@outlook.com>
2026-01-22 12:17:37 -0500

    Adding Author Name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
-// Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Simple C++ program to display "Hello World"
+//Author: Trevor Lee <hk8245@wayne.edu>
+// Header file for input output functions
 #include<iostream>  
   
 using namespace std; 
